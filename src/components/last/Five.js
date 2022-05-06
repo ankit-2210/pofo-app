f2b07329cf0c9a4d87a9731ed0f795032f5448c1
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2022-05-06 16:19:06 +0530

    backing

--- src/components/last/Five.js
+++ src/components/last/Five.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-import f from "../../images/1.jpg"
-import s from "../../images/2.jpg"
-import t from "../../images/3.jpg"
+
 
 class Five extends Component {
     render() {
