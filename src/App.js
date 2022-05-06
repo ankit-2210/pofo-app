8cb041b01552987cdc744420f20376ee7064a8ff
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2022-05-06 20:22:45 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css';
 import Home from './components/Home';
+import { Route, Switch } from "react-router-dom"
 
 function App() {
   return (
-    <div >
-      <Home />
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
