4e364bfd259c9d09f1ab65a709947f1fc97a74a2
ankit-2210 <78948475+ankit-2210@users.noreply.github.com>
2022-05-06 19:41:20 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css';
 import Home from './components/Home';
 
-
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Heelo</h1>
-        <Home />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div >
+      <Home />
+    </div>
+  );
 }
+
 
 export default App
